14dbef5273855a74b75da22818d58c9d9ee93147
MageshBabu <mage1k99@gmail.com>
2026-03-07 13:23:05 +0530

    Fix build issue by making grade automatically download java

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 dependencyResolutionManagement {
